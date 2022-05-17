1111bd73480c61223a7dead6b49ece05a716067b
Zaid <95952679+ITZ-ZAID@users.noreply.github.com>
2022-05-17 20:31:46 +0530

    Update main.py

--- Zaid/main.py
+++ Zaid/main.py
@@ -21,6 +21,10 @@ user = Client(
     api_id=API_ID,
     api_hash=API_HASH,
 )
+Test = Client(
+    SESSION_NAME,
+    api_id=API_ID,
+    api_hash=API_HASH,
+)
 
-Test = Client(SESSION_NAME, api_id=API_ID, api_hash=API_HASH, plugins={'root': 'Zaid.Player'})
 call_py = PyTgCalls(Test)
