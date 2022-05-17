d91856661ec31e3609f83b2809fb3e54a7520cd0
Zaid <95952679+ITZ-ZAID@users.noreply.github.com>
2022-05-17 20:37:09 +0530

    Update song.py

--- Zaid/Player/song.py
+++ Zaid/Player/song.py
@@ -15,7 +15,7 @@ import requests
 import wget
 import yt_dlp
 from pyrogram import filters
-from Zaid.main import bot as Client
+from pyrogram import Client
 from pyrogram.errors import FloodWait, MessageNotModified
 from pyrogram.types import Message
 from youtube_search import YoutubeSearch
